26a7f9108367f0ddfeba8aecc2c60dbc44b4d3ef
jpecanha <jefsp@hotmail.com>
2016-11-06 00:20:20 -0200

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express()
+var CONTACTS_COLLECTION = "contacts";
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+
+app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
