bae7a0c09f377f01bb971e7d26c2a3e9a211cceb
jpecanha <jefsp@hotmail.com>
2016-11-05 22:49:28 -0200

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,4 @@ var app = express()
 
 // CONTACTS API ROUTES BELOW
 
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
-
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
-app.get("/contacts", function(req, res) {
-});
-
-app.post("/contacts", function(req, res) {
-});
 
