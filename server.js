4090ddb7bf4c4b3d8782a1972d021c81391c8de8
jpecanha <jefsp@hotmail.com>
2016-11-06 00:40:36 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-//var firebase = require('firebase');
+var firebase = require("firebase");
 
 var app = express()
 var CONTACTS_COLLECTION = "contacts";
