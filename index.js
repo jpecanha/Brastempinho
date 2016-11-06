59cc33cc7afee6b5e041b1e9ef8d981adb642b27
jpecanha <jefsp@hotmail.com>
2016-11-05 22:48:33 -0200

    Update index.js

--- index.js
+++ index.js
@@ -20,17 +20,3 @@ app.get("/contacts", function(req, res) {
 app.post("/contacts", function(req, res) {
 });
 
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-app.get("/contacts/:id", function(req, res) {
-});
-
-app.put("/contacts/:id", function(req, res) {
-});
-
-app.delete("/contacts/:id", function(req, res) {
-});
