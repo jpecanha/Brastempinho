9b5a6038666f87ea63e9bef02bf12f52619de3c7
jpecanha <jefsp@hotmail.com>
2016-11-06 00:12:41 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-//var mongodb = require("mongodb");
-//var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-var app = express();
-//app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-//var db;
-
-// Connect to the database before starting the application server.
-/*mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-*/
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
