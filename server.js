545c7e65104c9909e399adcd4562f85c6485ae78
jpecanha <jefsp@hotmail.com>
2016-11-05 22:03:06 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-
-app.listen(3000);
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.JS!');
+}).listen(8080);
+console.log('Server running at http://localhost:8080/');
