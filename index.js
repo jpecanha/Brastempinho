9411879f6818ef24de3e3bdd0637b98616408afa
jpecanha <jefsp@hotmail.com>
2016-11-11 18:21:53 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/salesforce');
+//module.exports = require('./lib/salesforce');
+var sf = require('node-salesforce');
+var conn = new sf.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env. 
+   loginUrl : 'https://test.salesforce.com' 
+});
+conn.login(username, password, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  // Now you can get the access token and instance URL information. 
+  // Save them to establish connection next time. 
+  console.log(conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property 
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+  // ... 
+});
